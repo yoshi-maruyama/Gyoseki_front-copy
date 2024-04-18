33a0c6d617426c4fa269237da506c5ddbb4230fe
yoshihiro <yoshi.maruyama02@gmail.com>
2024-04-18 18:01:54 +0900

    Implemented add error icon at input text form

--- src/components/input-text/input-text.tsx
+++ src/components/input-text/input-text.tsx
@@ -1,18 +1,22 @@
 import classes from "@/components/input-text/input-text.module.scss";
 import { InputTextProps } from "@/components/input-text/types";
 import { FieldValues } from "react-hook-form";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
 
 export default function InputText<T extends FieldValues>(props: InputTextProps<T>) {
   const { label, placeholder, error, register, path } = props;
   return (
     <div>
       <label className={classes.input_label}>{label}</label>
-      <input
-        className={`${classes.input_text} ${error && classes.error}`}
-        placeholder={placeholder}
-        type="text"
-        {...register(path)}
-      />
+      <div className={classes.relative}>
+        <input
+          className={`${classes.input_text} ${error && classes.error}`}
+          placeholder={placeholder}
+          type="text"
+          {...register(path)}
+        />
+        {error && <ExclamationCircleOutlined className={classes.error_icon} />}
+      </div>
       {error && <div className={classes.error_text}>{error.message}</div>}
     </div>
   );
