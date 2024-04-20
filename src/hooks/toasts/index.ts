f7590b3b2d089c278ee193878fbe99404f63810d
yoshihiro <yoshi.maruyama02@gmail.com>
2024-04-20 12:34:12 +0900

    Implemented toaster custom hook

---
+++ src/hooks/toasts/index.ts
@@ -0,0 +1,28 @@
+import { Bounce, ToastOptions, toast } from "react-toastify";
+
+class AppToast {
+  private _settings: ToastOptions;
+
+  constructor(options?: ToastOptions) {
+    const defaultSettings = {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    };
+    this._settings = Object.assign({}, defaultSettings, options);
+  }
+
+  error(message: string) {
+    toast.error(message, { ...this._settings });
+  }
+}
+
+export default function useToast(options?: ToastOptions) {
+  return new AppToast(options);
+}
