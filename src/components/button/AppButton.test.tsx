666b35d86dc618906fb8bfa6f22f7ac9975059ee
yoshihiro <yoshi.maruyama02@gmail.com>
2024-04-23 17:46:04 +0900

    Integrated remove app button

--- src/components/button/AppButton.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import AppButton from "./AppButton";
-
-test("AppButton", () => {
-  render(<AppButton />);
-  expect(screen.getByRole("button")).toBeInTheDocument();
-});
--- src/components/button/AppButton.tsx
+++
@@ -1,3 +0,0 @@
-export default function AppButton() {
-  return <button>press</button>;
-}
