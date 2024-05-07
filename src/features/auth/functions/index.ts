25f0ef680ae2215a2cfa7295e3a555252a5f4a46
丸山祥弘 <yoshihiromaruyama@maruyamayoshihironoMacBook-Pro.local>
2024-05-07 17:27:56 +0900

    first commit

--- src/features/auth/functions/index.ts
+++ src/features/auth/functions/index.ts
@@ -17,7 +17,7 @@ export const submitHandler = async (
 
   if (!result?.error) {
     // TODO: callback urlへ遷移させる
-    router.replace("/");
+    router.replace("http://localhost:3000/menus");
   }
 
   if (result?.status === StatusCode.unauthorized) {
